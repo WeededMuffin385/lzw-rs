37324f6d884f4a6a51bf70e3f6588f6b1ff35663
WeededMuffin385 <zagoruiko.mihail@mail.ru>
2024-05-05 15:03:41 +0300

    Parallel algorithm simplification

--- src/lzw/parallel.rs
+++ src/lzw/parallel.rs
@@ -1,47 +1,28 @@
-use std::collections::HashSet;
 use std::mem::size_of;
-use std::ops::Not;
-use std::sync::mpsc::channel;
 use bitvec::field::BitField;
 use bitvec::order::Lsb0;
 use bitvec::prelude::{BitSlice, BitVec};
 use rayon::ThreadPoolBuilder;
 use crate::lzw::{decode, encode};
 
-const CHUNK_SIZE_MEGABYTES: usize = 32;
+const CHUNK_SIZE_MEGABYTES: usize = 48;
 const USIZE_BIT_SIZE: usize = size_of::<usize>() * 8;
 
 pub fn encode_parallel(data: &[u8]) -> BitVec {
 	let pool = ThreadPoolBuilder::new().num_threads(8).build().unwrap();
-	let (sender, receiver) = channel();
-
-	let mut len = 0;
+	let mut data: Vec<_> = data.chunks(2usize.pow(20) * CHUNK_SIZE_MEGABYTES).map(|data|(data, BitVec::default())).collect();
 
 	pool.scope(|scope|{
-		for (index, data) in data.chunks(2usize.pow(20) * CHUNK_SIZE_MEGABYTES).enumerate() {
-			len += 1;
-			let sender = sender.clone();
-
-			scope.spawn(move |_| {
-				sender.send((encode(data), index)).unwrap();
+		for (data, result) in &mut data {
+			scope.spawn(|_| {
+				*result = encode(data);
 			});
 		}
-		println!("chunks: {len}");
 	});
 
 
-	let mut set: HashSet<_> = (0..len).collect();
-	let mut results = vec![Default::default(); len];
-
-	while set.is_empty().not() {
-		if let Ok((data, index)) = receiver.try_recv() {
-			results[index] = data;
-			set.remove(&index);
-		}
-	}
-
 	let mut result = BitVec::new();
-	for value in results {
+	for (_,value) in data {
 		result.extend_from_bitslice(&BitVec::<usize, Lsb0>::from_element(value.len()));
 		result.extend_from_bitslice(&value);
 	}
@@ -52,38 +33,25 @@ pub fn encode_parallel(data: &[u8]) -> BitVec {
 
 
 pub fn decode_parallel(mut data: &BitSlice) -> Vec<u8> {
+	let pool = ThreadPoolBuilder::new().num_threads(8).build().unwrap();
+
 	let mut chunks = Vec::new();
 	while data.len() >= USIZE_BIT_SIZE {
 		let len = data[0..USIZE_BIT_SIZE].load::<usize>();
 		data = &data[USIZE_BIT_SIZE..data.len()];
-		chunks.push(data[0..len].to_bitvec());
+		chunks.push(&data[0..len]);
 		data = &data[len..data.len()];
 	}
 
-	let pool = ThreadPoolBuilder::new().num_threads(8).build().unwrap();
-	let (sender, receiver) = channel();
+	let mut data: Vec<_> = chunks.iter().map(|data|(data, Vec::default())).collect();
 
 	pool.scope(|scope|{
-		for (index, data) in chunks.iter().enumerate() {
-			let sender = sender.clone();
-
+		for (data, result) in &mut data {
 			scope.spawn(move |_| {
-				sender.send((decode(data), index)).unwrap();
+				*result = decode(data);
 			});
 		}
 	});
 
-
-	let len = chunks.len();
-	let mut set: HashSet<_> = (0..len).collect();
-	let mut results = vec![Default::default(); len];
-
-	while set.is_empty().not() {
-		if let Ok((data, index)) = receiver.try_recv() {
-			results[index] = data;
-			set.remove(&index);
-		}
-	}
-
-	results.concat()
+	data.iter().flat_map(|(_, value)|value.clone()).collect()
 }
